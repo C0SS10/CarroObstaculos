b6b42ef0b746765bcdd200c74eb0d4ca58baa899
E-CG <57926268+E-CG@users.noreply.github.com>
2023-04-25 09:27:49 -0500

    Update main.py
    Condiciones para el movimiento del carro.

--- src/main.py
+++ src/main.py
@@ -11,6 +11,15 @@ def main() -> None:
             if sensor.pothole():
                 print("Definitivamente, Hay un hueco")
             time.sleep(1)
+            
+            if condition1: #Si hay un muro
+                Movimiento.move_forward()
+            elif condition2: #Si hay un hueco
+                Movimiento.move_backward()
+            elif condition3: #Si hay una escalera
+                Movimiento.turn_left()
+            else:
+                Movimiento.stop()
 
     except KeyboardInterrupt:
         print('Ha habido una interrupción')
